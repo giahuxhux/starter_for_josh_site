b6e4bbc410743b095d77fcd8ccd135f1842aa457
LuanKovacs <49140495+LuanKovacs@users.noreply.github.com>
2020-03-12 13:09:44 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer class="container-fluid"> <!-- div for the main image to stretch to the edge of page-->
+          <div class="container"> <!-- div to hold the content in the middle of the page-->
+              <div class="row"> <!-- a row that gives us access to the BS columns-->
+                  <div class="col-md-6 text-center footer-content">
+                    <p><?php bloginfo('name'); ?></p>
+                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpeg" alt="tumblr">
+                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt="rss">
+                  </div>
+                  <div class="col-md-6 text-center footer-content">
+                    <p>Terms and Conditions</p> 
+                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/linkedin.jpg" alt="linkedin">
+                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/facebook.jpg" alt="facebook">
+                  </div>
+                  
+              
+              </div> <!--row-->
+          </div> <!--container-->
+      </footer>
+<?php wp_footer(); ?>
