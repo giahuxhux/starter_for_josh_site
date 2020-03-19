a77a9e586f6b3dc0e335954d4cba044e5bceb478
LuanKovacs <49140495+LuanKovacs@users.noreply.github.com>
2020-03-19 16:10:07 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
               <div class="row"> <!-- a row that gives us access to the BS columns-->
                   <div class="col-md-6 text-center footer-content">
                     <p><?php bloginfo('name'); ?></p>
-                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpeg" alt="tumblr">
-                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt="rss">
+                      <img src="http://206.189.45.97/~mesh17/wp-content/themes/starter_for_josh_site/images/tumblr.jpeg" alt="tumblr">
+                      <img src="http://206.189.45.97/~mesh17/wp-content/themes/starter_for_josh_site/images/rss.jpg" alt="rss">
                   </div>
                   <div class="col-md-6 text-center footer-content">
                     <p>Terms and Conditions</p> 
-                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/linkedin.jpg" alt="linkedin">
-                      <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/facebook.jpg" alt="facebook">
+                      <img src="http://206.189.45.97/~mesh17/wp-content/themes/starter_for_josh_site/images/linkedin.jpg" alt="linkedin">
+                      <img src="http://206.189.45.97/~mesh17/wp-content/themes/starter_for_josh_site/images/facebook.jpg" alt="facebook">
                   </div>
                   
               
