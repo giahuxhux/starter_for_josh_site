01b0443deab3c954a9770ec6271b628a7f2960f9
LuanKovacs <49140495+LuanKovacs@users.noreply.github.com>
2020-04-23 12:18:56 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -9,7 +9,10 @@
         <div class="container">
             
 <!-- placeholder for aboutme -->
-              <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+            <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+            <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+            <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
+
             
             <div class="blocker"></div> 
             
