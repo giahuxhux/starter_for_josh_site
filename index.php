9bc34b3e25ca9a6c00a26ca21447561eeb06b5c8
giahuxhux <64679534+giahuxhux@users.noreply.github.com>
2021-04-19 13:08:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 <section class="container-fluid aboutbg text-center">
 
-<?php
+    <?php
 global $more;//define a global variable
 $more = 0;// the global varibale is now equal to 0
 query_posts('cat=5');//look for posts that have the category of 2
 if(have_posts()) ://if we have posts to display
 while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
 ?>
-<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2> 
-            <div><p class="about-text"><?php the_content() ?></p></div>
-<?php
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+    <div>
+        <p class="about-text"><?php the_content() ?></p>
+    </div>
+    <?php
 endwhile;
 endif;
-wp_reset_query();?>    
+wp_reset_query();?>
 
 
-        <div class="blocker">
+    <div class="blocker">
 
 
-        </div>
-        <!--blocker-->
+    </div>
+    <!--blocker-->
     </div>
     <!--container-->
 </section>
@@ -34,46 +36,45 @@ wp_reset_query();?>
             </div>
 
             <div class="col-md-4 article-cent">
-                <img class="img-responsive" alt="portrait" src="images/person.jpg">
-                <h4> PORTRAIT</h4>
-                <p class="date">12 December 2020</p>
+<?php $post_id = 174;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?>
 
-                <p>
-                    STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
-                <p class="readmore">CONTINUE READING</p>
+
+                <div class="col-md-4 article-cent">
+                    <img class="img-responsive" alt="portrait" src="images/person.jpg">
+                    <h4> PORTRAIT</h4>
+                    <p class="date">12 December 2020</p>
+
+                    <p>
+                        STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
+                    <p class="readmore">CONTINUE READING</p>
+                </div>
+
+                <div class="col-md-4 article-cent">
+                    <img class="img-responsive" alt="portrait" src="images/person.jpg">
+                    <h4> PORTRAIT</h4>
+                    <p class="date">12 December 2020</p>
+                    <p>
+                        STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
+                    <p class="readmore">CONTINUE READING</p>
+                </div>
+
+
+
+
+
+
+
+
+
+
             </div>
-
-            <div class="col-md-4 article-cent">
-                <img class="img-responsive" alt="portrait" src="images/person.jpg">
-                <h4> PORTRAIT</h4>
-                <p class="date">12 December 2020</p>
-
-                <p>
-                    STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
-                <p class="readmore">CONTINUE READING</p>
-            </div>
-
-            <div class="col-md-4 article-cent">
-                <img class="img-responsive" alt="portrait" src="images/person.jpg">
-                <h4> PORTRAIT</h4>
-                <p class="date">12 December 2020</p>
-                <p>
-                    STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
-                <p class="readmore">CONTINUE READING</p>
-            </div>
-
-
-
-
-
-
-
-
-
-
+            <!--row-->
         </div>
-        <!--row-->
-    </div>
-    <!--container-->
+        <!--container-->
 </section>
 <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
