15948e18748ac98db9966b4a1b3767622827f486
giahuxhux <64679534+giahuxhux@users.noreply.github.com>
2021-04-19 14:30:57 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -1,69 +1,52 @@
 <?php
-    /*
-    Template Name: Home page
-    */
-    ?>
+/*
+Template Name: home page
+*/
+?>
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 <section class="container-fluid aboutbg text-center">
-      
-        <div class="container">
-            
-<!-- placeholder for aboutme -->
-            <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
-            <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
-            <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
 
-            
-            <div class="blocker"></div> 
-            
-            
+<!--placeholder for aboutme-->
+
+
+    <div class="blocker">
+
+
+    </div>
+    <!--blocker-->
+    </div>
+    <!--container-->
+</section>
+
+<section class="container-fluid articlebg ">
+    <div class="container">
+        <div class="row">
+
+            <div>
+                <h3 class="text-center article-title"> LATEST ARTICLES</h3>
             </div>
-            <!-- container-->
-      
-      </section>
-      
-      
-      
-      <section class="container-fluid articlebg">
-        <div class="container">
-            <div class="row"> <!-- a row that gives us access to the BS columns-->
-                
-                <div>
-                <h3 class="text-center article-title "><?php the_field('titleforarticles'); ?></h3>
-                </div>
-                
-                    <div class="col-md-4 article-cent">
-                        
-<img class="img-responsive" alt=“portait” src="<?php the_field('articleimage'); ?>">
-<h4><?php the_field('articletitle'); ?></h4>
-                        <p><?php the_field('articleexcerpt'); ?></p>
-                            <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
 
-<!-- placeholder for col1 -->
-                
-                    </div>
-                
-                     <div class="col-md-4 article-cent">
+            <div class="col-md-4 article-cent">
+<!--placeholder for col1-->
+ </div>
 
-<img class="img-responsive" alt=“model” src="<?php the_field('articleimagetwo'); ?>">
-<h4><?php the_field('articletitletwo'); ?></h4>
-                        <p><?php the_field('articleexcerpttwo'); ?></p>
-                            <a href="<?php the_field('readmoretwo'); ?>" class="readmore">CONTINUE READING</a>
-<!-- placeholder for col1 -->
+                <div class="col-md-4 article-cent">
+<!--placeholder for col2-->   
+ </div>
 
-                    </div>
-                
-                     <div class="col-md-4 article-cent">
-                         
-<img class="img-responsive" alt=“gamedesign” src="<?php the_field('articleimagethree'); ?>">
-<h4><?php the_field('articletitlethree'); ?></h4>
-                        <p><?php the_field('articleexcerptthree'); ?></p>
-                            <a href="<?php the_field('readmorethree'); ?>" class="readmore">CONTINUE READING</a>
-<!-- placeholder for col1 -->
+                <div class="col-md-4 article-cent">
+<!--placeholder for col3-->
+ </div>
 
-                    </div>
-            
-            </div> <!-- row-->
-        </div> <!-- container-->
-      </section>
+
+
+
+
+
+
+            </div>
+            <!--row-->
+        </div>
+        <!--container-->
+</section>
 <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
