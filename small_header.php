ef847d6e3db997cd0667562793063fc362644489
giahuxhux <64679534+giahuxhux@users.noreply.github.com>
2021-04-19 13:49:14 +0800

    Update small_header.php

--- small_header.php
+++ small_header.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">"
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    
-      <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
       
       <title><?php
 		      if (function_exists('is_tag') && is_tag()) {
@@ -29,47 +28,51 @@
 </title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
+   
+   
+    
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,800&display=swap" rel="stylesheet">  
-    <link rel="stylesheet" href="style.css" />
-    <link rel="stylesheet" href="responsive.css" />
-<?php wp_head(); ?> 
-  </head>
-    
-  <body>
-         <header class="container-fluid small_herobg"> <!-- div for the main image that stretches to the edge of the page-->
-      <nav class="navbar">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WITKIN</span></a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav navbar-right">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">PROJECTS</a></li>
-        <li><a href="#">PUBLICATIONS</a></li>
-          <li><a href="#">CV</a></li>
-          <li><a href="#">CONTACT</a></li>
-      </ul>
-      
-    </div>
-  </div>
-</nav>
-      
-      
-        <div class="container"> <!--div that holds the content in the middle of the page-->
-        </div> <!--container-->
-      
-      </header>
+  <?php wp_head(); ?> 
+    </head>
+
+<body>
+    <header class="container-fluid small_herobg">
+        <nav class="navbar">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+
+
+
+                   <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">PROJECTS</a></li>
+                        <li><a href="#">PUBLICATIONS</a></li>
+                        <li><a href="#">CV</a></li>
+                        <li><a href="#">CONTACT</a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+
+
+
+        <div class="container">
+        </div>
+        <!--Container-->
+
+    </header>
