c3e4a1b5dd538457635d4b5916f0a939c42a0ce7
giahuxhux <64679534+giahuxhux@users.noreply.github.com>
2021-04-19 12:38:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,73 +1,71 @@
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 <section class="container-fluid aboutbg text-center">
-      
-        <div class="container">
-<?php
-global $more;//define a global variable
-$more = 0;// the global varibale is now equal to 0
-query_posts('cat=2');//look for posts that have the category of 2
-if(have_posts()) ://if we have posts to display
-while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
-?>
-<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2> 
-            <div><p class="about-text"><?php the_content() ?></p></div>
-<?php
-endwhile;
-endif;
-wp_reset_query();?>    
 
-            
-            <div class="blocker"></div> 
-            
-            
-            
-            </div><!-- container-->
-      
-      </section>
-      
-      
-      
-      <section class="container-fluid articlebg">
-        <div class="container">
-            <div class="row"> <!-- a row that gives us access to the BS columns-->
-                <div>
-                <h3 class="text-center article-title">LATEST ARTICLES</h3>
-                </div>
-                
-                    <div class="col-md-4 article-cent">
-                        
-<?php $post_id = 12;
-$queried_post = get_post($post_id);?>
-<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
-<h4><?php echo $queried_post->post_title; ?></h4>
-<?php echo $queried_post->post_excerpt; ?>
-<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?>
-                
-                    </div>
-                
-                     <div class="col-md-4 article-cent">
+    <div class="container">
+        <h2> GAMES & LEVEL DESIGNER </h2>
+        <p class="particle"> and particle wizard</p>
+        <p class="abouttext">My name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments.
+            I always tell a story.</p>
 
-<?php $post_id = 14;
-$queried_post = get_post($post_id);?>
-<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
-<h4><?php echo $queried_post->post_title; ?></h4>
-<?php echo $queried_post->post_excerpt; ?>
-<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?>
 
-                    </div>
-                
-                     <div class="col-md-4 article-cent">
+        <div class="blocker">
 
-<?php $post_id = 16;
-$queried_post = get_post($post_id);?>
-<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
-<h4><?php echo $queried_post->post_title; ?></h4>
-<?php echo $queried_post->post_excerpt; ?>
-<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?>
 
-                    </div>
-            
-            </div> <!-- row-->
-        </div> <!-- container-->
-      </section>
+        </div>
+        <!--blocker-->
+    </div>
+    <!--container-->
+</section>
+
+<section class="container-fluid articlebg ">
+    <div class="container">
+        <div class="row">
+
+            <div>
+                <h3 class="text-center article-title"> LATEST ARTICLES</h3>
+            </div>
+
+            <div class="col-md-4 article-cent">
+                <img class="img-responsive" alt="portrait" src="images/person.jpg">
+                <h4> PORTRAIT</h4>
+                <p class="date">12 December 2020</p>
+
+                <p>
+                    STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
+                <p class="readmore">CONTINUE READING</p>
+            </div>
+
+            <div class="col-md-4 article-cent">
+                <img class="img-responsive" alt="portrait" src="images/person.jpg">
+                <h4> PORTRAIT</h4>
+                <p class="date">12 December 2020</p>
+
+                <p>
+                    STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
+                <p class="readmore">CONTINUE READING</p>
+            </div>
+
+            <div class="col-md-4 article-cent">
+                <img class="img-responsive" alt="portrait" src="images/person.jpg">
+                <h4> PORTRAIT</h4>
+                <p class="date">12 December 2020</p>
+                <p>
+                    STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
+                <p class="readmore">CONTINUE READING</p>
+            </div>
+
+
+
+
+
+
+
+
+
+
+        </div>
+        <!--row-->
+    </div>
+    <!--container-->
+</section>
 <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
